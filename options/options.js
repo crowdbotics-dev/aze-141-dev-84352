4ed762e7d448f6d79667b2fc3286173f08a92a83
abdelhak@crowdbotics.com <abdelhak@crowdbotics.com>
2023-05-18 14:51:58 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "aze-141-dev-84352",
-  url: "https://aze-141-dev-84352.botics.co",
-  api: "https://aze-141-dev-84352.botics.co/api/v1"
+  "name": "aze-141-dev-84352",
+  "url": "https://aze-141-dev-84352.botics.co",
+  "api": "https://aze-141-dev-84352.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
