9eee84760358189b18962e98e86d8c26c4b1f47e
abdelhak@crowdbotics.com <abdelhak@crowdbotics.com>
2023-05-18 17:46:52 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,39 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { View } from "react-native";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
+  const navigation = useNavigation();
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}>
+        <Pressable onPress={() => {
+        navigation.navigate("Youtube Player");
+      }}><View style={styles.Jkavbodw}>
+          Button
+        </View></Pressable>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  Jkavbodw: {
+    height: 60,
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#281ac3",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled1;
